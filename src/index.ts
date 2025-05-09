b4cb0b4d00f0b2a78f5269a9afd92fb06d91a32c
Brandon Ly <brandonly@lostcoding.com>
2025-05-08 19:27:44 -0500

    add dist

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import * as core from "@actions/core";
 
 async function main() {
 	const modelVersion = core.getInput("MODEL_VERSION");
+
+	core.debug(`The model version is: ${modelVersion}`);
 }
 
 main();
